de6c7b058e914f83b5b50da4b07696f69f59a49e
Uttam1910 <uttamt1202@gmail.com>
2024-07-04 13:25:59 +0530

    Set up Redux with authSlice and store

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
 import { Toaster } from 'react-hot-toast';
+import store from './redux/store';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-    <App />
-    <Toaster/>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slice/authSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  devTools: true, // Enable Redux DevTools
+});
+
+export default store;
